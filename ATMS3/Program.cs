dd0053bfa34b91a5aef7af4b596b08f30adeb566
Admin <Admin@YEL>
2024-04-12 16:17:39 +0800

    Add project files.

---
+++ ATMS3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ATMlibrary3;
+namespace ATMS3
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM3.Main(args);
+        }
+    }
+}
